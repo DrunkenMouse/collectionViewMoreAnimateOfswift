578ea30933a9038a98bf61de029796be68ae6c05
残月 <632342527@qq.com>
2016-07-19 14:39:50 +0800

    Initial Commit

---
+++ collectionView的多种转场动画swift版/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  collectionView的多种转场动画swift版
+//
+//  Created by 王奥东 on 16/7/19.
+//  Copyright © 2016年 王奥东. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ collectionView的多种转场动画swift版Tests/collectionView_______swift_Tests.swift
@@ -0,0 +1,36 @@
+//
+//  collectionView_______swift_Tests.swift
+//  collectionView的多种转场动画swift版Tests
+//
+//  Created by 王奥东 on 16/7/19.
+//  Copyright © 2016年 王奥东. All rights reserved.
+//
+
+import XCTest
+@testable import collectionView_______swift_
+
+class collectionView_______swift_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
